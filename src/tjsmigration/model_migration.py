65189c6aabc0b02fcb3ff0914f3527acfa1cd2f2
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-07-01 17:16:00 +0900

    Store failed result as well

--- src/tjsmigration/model_migration.py
+++ src/tjsmigration/model_migration.py
@@ -301,6 +301,7 @@ def migrate_model_files(hf_api: HfApi, model_info: ModelInfo, working_dir: Path,
             continue
         quantization_config.slim = True
         result = call_quantization_script(hf_api=hf_api, model_info=model_info, quantization_config=quantization_config, working_dir=working_dir, output_dir=output_dir)
+        results.append(result)
         if result.error and fallback_to_no_slimming:
             logger.warning(f"Failed to quantize {quantization_config.base_model.stem} with slimming. Retrying without slimming...")
             quantization_config.slim = False
